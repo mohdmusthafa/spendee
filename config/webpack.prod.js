b63d366921bfe44fbb0f3854061c0df0c3001435
Mohammed Musthafa <muhammed.mustafa@emvigotech.com>
2021-01-01 12:13:00 +0530

    webpack configurations splitted

---
+++ config/webpack.prod.js
@@ -0,0 +1,99 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const Dotenv = require('dotenv-webpack')
+const path = require('path')
+
+module.exports = {
+    entry: ['react-hot-loader/patch', './src/index.js'],
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+      chunkFilename: '[id].css',
+        }),
+        new Dotenv({
+            path: './config/.env.production',
+            safe: true
+        })
+    ],
+    resolve: {
+        modules: [__dirname, 'src', 'node_modules'],
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: require.resolve('babel-loader'),
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: ''
+                        }
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                use: ['url-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin()
+        ],
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendors",
+                    chunks: "all"
+                }
+            }
+        }
+    },
+    devServer: {
+        host: 'localhost',
+        publicPath: '/',
+        port: 3002
+    }
+};
